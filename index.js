a828a9a03c65c65065f83061e579499da36bb19f
Marco Fugaro <marco.fugaro@gmail.com>
2018-05-14 15:02:14 +0200

    💥 Bang! First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const tachyonsGenerator = require('tachyons-generator')
+const config = {
+  customMedia: [],
+  colors: {
+    black: '#000',
+    white: '#fff',
+  },
+  skipModules: ['normalize'],
+}
+
+const generate = async () => {
+  const output = await tachyonsGenerator(config).generate()
+
+  fs.writeFileSync('tachyons-lite.css', output.css)
+  fs.writeFileSync('tachyons-lite.min.css', output.min)
+}
+
+generate()
